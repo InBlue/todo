98e029bad5e85e6cd4565c36541ca52278eba81b
InBlue <InBlueOfficial@Outlook.com>
2020-11-15 15:44:12 +1000

    :sparkles: Ability to add tasks and mark/unmark them as complete

--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -6,12 +6,11 @@ export default function TodoItem(props) {
 
     const changeToTick = (v) => {
         setShowTick(v);
-        console.log(v);
     }
 
     return (
-        <div className="todo-item">
-            <div className="todo-action">
+        <div id={props.id} className="todo-item">
+            <div className="todo-action" onClick={() => props.toggle(props.id)}>
                 {!showTick ? <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#607D8B" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="9" /></svg> : <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#607D8B" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="9" /><path d="M9 12l2 2l4 -4" /></svg> }
             </div>
             <h3 className="todo-item-task">{props.name}</h3>
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,22 +1,67 @@
+import { useState, useEffect } from 'react';
+
 import '../assets/scss/home.scss';
+import TodoItem from '../components/todoItem';
+import usePersistedState from '../util/usePersistedState.js';
 
 export default function Home() {
+	const [itemText, setItemText] = useState("");
+	const [items, setItems] = usePersistedState('todo-items', []); // Using the custom localStorage handler
+
+	const [incompleteItems, setIncompleteItems] = useState([]);
+	const [completeItems, setCompleteItems] = useState([]);
+
+	useEffect(() => {
+		items.forEach(item => {
+			if(!item.complete) return setIncompleteItems(oldIncompleteItems => [...oldIncompleteItems, item]);
+			if(item.complete) return setCompleteItems(oldCompleteItems => [...oldCompleteItems, item]);
+		});
+		return () => {
+			setIncompleteItems([]);
+			setCompleteItems([]);
+		}
+	}, [items]);
+
+	const addItem = () => {
+		const item = {
+			text: itemText,
+			complete: false,
+			id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+		}
+		setItems(oldItems => [...oldItems, item]);
+	}
+
+	const toggleComplete = (id) => {
+		const oldItems = items;
+		const updatedItems = oldItems.filter(item => item.id !== id);
+		const item = oldItems.filter(item => item.id === id);
+		item[0].complete = !item[0].complete;
+		setItems(updatedItems);
+		setItems(oldItems => [...oldItems, item[0]]);
+	}
+
 	return (
 		<div id="home"> {/* React requires all components to be surrounded by a parent element */}
-    		<div id="todo-header"> {/* I like to add prefixes to my class names and ids when using react. This is because when compiled, the stylesheets are combined and it is easy to confuse which style to apply. */}
-				<h1 id="todo-title">todo.</h1>
-				<div id="todo-new">
-					<input id="todo-input" type="text" name="todo-item" />
-					<p id="todo-add">Add</p>
+			<div id="todo">
+				<div id="todo-header"> {/* I like to add prefixes to my class names and ids when using react. This is because when compiled, the stylesheets are combined and it is easy to confuse which style to apply. */}
+					<h1 id="todo-title">todo.</h1>
+					<div id="todo-new">
+						<input id="todo-input" type="text" name="todo-item" onChange={(e) => setItemText(e.target.value.toString().trim())} />
+						<p id="todo-add" onClick={() => addItem()}>Add</p>
+					</div>
 				</div>
-      		</div>
 				<div id="todo-tasks">
 					<div id="todo-incomplete">
-						<h3 className="todo-tasks-title">Incomplete Tasks</h3>
-						<TodoItem name="Make todo list" />
+						{incompleteItems.length >= 1 ? <h3 className="todo-tasks-title">Incomplete Tasks</h3> : ''}
+						{incompleteItems.map((item, i) => (
+							<TodoItem key={i} id={item.id} name={item.text} toggle={toggleComplete} />
+						))}
 					</div>
 					<div id="todo-complete">
-						<h3 className="todo-tasks-title">Completed Tasks</h3>
+						{completeItems.length >= 1 ? <h3 className="todo-tasks-title">Completed Tasks</h3> : ''}
+						{completeItems.map((item, i) => (
+							<TodoItem key={i} id={item.id} name={item.text} toggle={toggleComplete} />
+						))}
 					</div>
 				</div>
 			</div>
