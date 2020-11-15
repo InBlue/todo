8b9063d85da6f2b0ff26c4f3047eb1c7268ebf52
InBlue <InBlueOfficial@Outlook.com>
2020-11-15 14:07:02 +1000

    :recycle: Changed the 'span' to a 'p'

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -5,7 +5,10 @@ export default function Home() {
 		<div id="home"> {/* React requires all components to be surrounded by a parent element */}
     		<div id="todo-header"> {/* I like to add prefixes to my class names and ids when using react. This is because when compiled, the stylesheets are combined and it is easy to confuse which style to apply. */}
 				<h1 id="todo-title">todo.</h1>
-				<input id="todo-add" type="text" name="todo-item" />
+				<div id="todo-new">
+					<input id="todo-input" type="text" name="todo-item" />
+					<p id="todo-add">Add</p>
+				</div>
       		</div>
     	</div>
   	);
